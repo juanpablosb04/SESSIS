d3d0f13b097d11b2636a384e58ac7fc681e24598
juanpablosb04 <juanpablosb04@hotmail.com>
2025-07-21 20:27:37 -0600

    Cambios estéticos a las paginas previamente listas

--- JS/navbar.js
+++ JS/navbar.js
@@ -8,16 +8,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const navbar = `
   <div class="navbar">
-    <div class="logo"><a href="#">SES</a></div>
+    <div class="logo"><a href="/index.html">SES</a></div>
     <ul class="links">
       <li><a href="/index.html">Inicio</a></li>
       <li><a href="/empleados.html">Empleados</a></li>
       <li><a href="/clientes.html">Clientes</a></li>
       <li class="dropdown">
-        <a href="/citas.html">Citas</a>
+        <a href="">Citas</a>
         <ul class="dropdown-menu">
-          <li><a href="">Registrar cita</a></li>
-          <li><a href="">Ver citas agendadas</a></li>
+          <li><a href="/registrarCitas.html">Registrar cita</a></li>
+          <li><a href="/verCitas.html">Ver citas agendadas</a></li>
         </ul>
       </li>
       <li><a href="/inventario.html">Inventario</a></li>
