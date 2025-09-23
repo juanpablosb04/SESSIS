0e94d0fa93fde766c29f269cf8c645733dedf4b2
juanpablosb04 <juanpablosb04@hotmail.com>
2025-09-22 20:40:07 -0600

    CRUD de Empleados Funcional
    Adelanto del Spring 2 con la sección de empleados

--- empleados/urls.py
+++ empleados/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('empleados/', views.registrar_empleado, name='empleados'),
+    path('empleados/editar/<int:id_empleado>/', views.editar_empleado, name='editar_empleado'),
+    path('empleados/eliminar/<int:id_empleado>/', views.eliminar_empleado, name='eliminar_empleado'),
 ]
