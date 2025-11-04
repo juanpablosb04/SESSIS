45cc97203ca6ab27ee2476b3a2ade71c450f84b2
StwartGF <114199940+StwartGF@users.noreply.github.com>
2025-11-03 19:49:19 -0600

    Funcionalidad de exportar en asistencia
    se puede exportar los datos de la tabla asistencia en Excel, y PDF, falta resolver problema de la vista del PDF estan muy grandes las columnas.

--- asistencia/urls.py
+++ asistencia/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('registrarAsistencia/', views.registrar_asistencia_view, name='registrarAsistencia'),
     path('consultarAsistencia/', views.asistencias_activas_view, name='consultarAsistencia'),
+    path('verAsistenciaOficiales/', views.ver_asistencia_oficiales_view, name='verAsistenciaOficiales'),
+    path('verAsistenciaOficiales/export/', views.ver_asistencia_oficiales_export, name='verAsistenciaOficialesExport'),
+    path('verAsistenciaOficiales/export/pdf/', views.ver_asistencia_oficiales_export_pdf, name='verAsistenciaOficialesExportPDF') # PDF
 ]
