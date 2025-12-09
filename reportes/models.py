3f5fe703275c934ccb3277141b8a9ee1ae32927f
juanpablosb04 <juanpablosb04@hotmail.com>
2025-12-09 14:29:21 -0600

    modelos update

--- reportes/models.py
+++ reportes/models.py
@@ -25,5 +25,5 @@ class ReporteIncidente(models.Model):
     descripcion  = models.TextField(blank=True, null=True)
 
     class Meta:
-        db_table = 'dbo.ReporteIncidente'
-        managed  = False
+        db_table = 'ReporteIncidentes'  # quitar 'dbo.'
+        managed = False
