04d93452e1facde9eceaf9f08c82062832e4a9a8
juanpablosb04 <juanpablosb04@hotmail.com>
2025-12-09 13:05:27 -0600

    Update

--- config/settings.py
+++ config/settings.py
@@ -58,8 +58,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -95,14 +95,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 USE_RAILWAY = os.getenv("RAILWAY_ENVIRONMENT")
 
 if USE_RAILWAY:
+    # ✅ Usando la DB pública para pruebas desde el contenedor de Railway
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.getenv("MYSQLDATABASE") or "railway",
-            'USER': os.getenv("MYSQLUSER") or "root",
-            'PASSWORD': os.getenv("MYSQLPASSWORD") or "MaLwpmXBzVTlAXMSVSSneZtrMSldKKZM",
-            'HOST': os.getenv("MYSQLHOST") or "shuttle.proxy.rlwy.net",
-            'PORT': os.getenv("MYSQLPORT") or "40663",
+            'NAME': os.getenv("MYSQLDATABASE", "railway"),
+            'USER': os.getenv("MYSQLUSER", "root"),
+            'PASSWORD': os.getenv("MYSQLPASSWORD", "MaLwpmXBzVTlAXMSVSSneZtrMSldKKZM"),
+            'HOST': os.getenv("RAILWAY_TCP_PROXY_DOMAIN", "shuttle.proxy.rlwy.net"),
+            'PORT': os.getenv("RAILWAY_TCP_PROXY_PORT", "40663"),
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",  # ayuda a evitar errores de integridad
+            },
         }
     }
 else:
@@ -160,11 +164,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Para que Django sepa dónde buscar tus archivos estáticos globales
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
