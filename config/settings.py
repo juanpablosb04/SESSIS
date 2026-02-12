6d4ff62694a83b11a598d8aa467b836d27563e96
juanpablosb04 <juanpablosb04@hotmail.com>
2026-02-11 20:25:28 -0600

    update

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://web-production-738e4.up.railway.app",
+    "https://web-production-c0d4b.up.railway.app",
 ]
 
 
@@ -94,21 +94,37 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Detecta si estamos en Railway
 USE_RAILWAY = os.getenv("RAILWAY_ENVIRONMENT")
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("MYSQLDATABASE", "railway"),
-        'USER': os.getenv("MYSQLUSER", "root"),
-        'PASSWORD': os.getenv("MYSQLPASSWORD"),
-        'HOST': os.getenv("MYSQLHOST", "mysql.railway.internal"),
-        'PORT': os.getenv("MYSQLPORT", "3306"),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+if USE_RAILWAY:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv("MYSQLDATABASE", "railway"),
+            'USER': os.getenv("MYSQLUSER", "root"),
+            'PASSWORD': os.getenv("MYSQLPASSWORD"),
+            'HOST': os.getenv("MYSQLHOST"),  # mysql.railway.internal
+            'PORT': os.getenv("MYSQLPORT", "3306"),
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            },
+        }
     }
-}
 
-
+else:
+    # 🔹 Local (SQL Server)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'mssql',
+            'NAME': 'SESSIS',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': 'AKILES08CR\\SQLEXPRESS',
+            'PORT': '',
+            'OPTIONS': {
+                'driver': 'ODBC Driver 17 for SQL Server',
+                'trusted_connection': 'yes',
+            },
+        }
+    }
 
 # Para Stwart : AKILES08CR\SQLEXPRESS
 
